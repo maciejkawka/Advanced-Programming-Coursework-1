068c7488e55cab7ac3595872229553e9db2a6978
Maciej Kawka <46383143+maciejkawka@users.noreply.github.com>
2020-11-04 18:06:34 +0000

    1

--- Maze.h
+++ Maze.h
@@ -26,6 +26,7 @@ private:
 	int IntRandom(int floor, int ceiling);
 	void SuffleArray(int*arra);
 	void MazeGenerator(Point step);
+	bool PatchFinder(Point step);
 	void ExitPointPlacement();
 	void CenterSquereGenerator();
 
